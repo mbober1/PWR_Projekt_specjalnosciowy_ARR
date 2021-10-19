e3e29b577f24401522f34db2bf60f5bc86e478b8
Marcin Bober <mbober1@gmail.com>
2021-10-19 11:20:09 +0200

    update

--- EDDA/edda.m
+++ EDDA/edda.m
@@ -6,3 +6,18 @@ G = 9.81;
 
 U1 = 0;
 U2 = 0;
+
+Kp = 100000;
+Kd = Kp/10;
+
+a = sim("model.slx");
+
+time = a.get('time');
+e1 = a.get('e1');
+e2 = a.get('e2');
+
+figure(1);
+plot(time, e1);
+
+figure(2);
+plot(time, e2);
