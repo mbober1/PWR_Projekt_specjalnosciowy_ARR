e0e0573678820cba2faf767d600e1203c0747208
Marcin Bober <mbober1@gmail.com>
2021-10-26 11:28:49 +0200

    update

--- EDDA/edda.m
+++ EDDA/edda.m
@@ -5,29 +5,46 @@ O4 = 0.77;
 G = 9.81;
 
 
-Kp = 10000000;
-Kd = Kp/10;
+%Kp = 10000000;
+%Kd = Kp/10;
 
-K0 = 1;
 K1 = 1;
 
-a = sim("qui_dorsey.slx");
 
-time = a.get('time');
-e1 = a.get('e1');
-e2 = a.get('e2');
 
-e11 = a.get('e11');
-e21 = a.get('e21');
+% e11 = a.get('e11');
+% e21 = a.get('e21');
 
-subplot(2,1,1);
-plot(time, e1);
+
+
+for K0 = 1:10:30
+    a = sim("exact_linearization_new.slx");
+
+    hold on;
+    subplot(2,1,1);
+    time = a.get('time');
+    e1 = a.get('e1');
+    plot(time, e1);
+end
+
 title('e_1(t)');
 xlabel('t[s]');
 ylabel('e_1[rad]');
+legend('K0=1','K0=10', 'K0=20')
 
-subplot(2,1,2);
-plot(time, e2);
+
+for K0 = 1:10:30
+    a = sim("exact_linearization_new.slx");
+
+    hold on;
+    subplot(2,1,2);
+    time = a.get('time');
+    e2 = a.get('e2');
+    plot(time, e2);
+end
+
+
 title('e_2(t)')
 xlabel('t[s]');
+legend('K0=1','K0=10', 'K0=20')
 ylabel('e_2[rad]');
