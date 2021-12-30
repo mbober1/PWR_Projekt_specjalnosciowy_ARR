40e01e85a395994cc7237574d91389976719173c
Marcin Bober <mbober1@gmail.com>
2021-12-30 11:41:31 +0100

    update

--- NH/astolfi_script.m
+++ NH/astolfi_script.m
@@ -1,17 +1,31 @@
+clear all;
+close all;
 
-a = sim("astolfi.slx");
+KM = 10;
+M11 = 107.9;
+M22 = 83.6;
 
-time = a.get('time');
-x1 = a.get('X');
-x2 = a.get('Y');
-x3 = a.get('Ey');
-x4 = a.get('Eo');
+kin = sim("astolfi_kin.slx");
+dyn = sim("astolfi_dyn.slx");
 
+time = kin.get('time');
+time2 = dyn.get('time');
+x1 = kin.get('X');
+x2 = kin.get('Y');
+x3 = dyn.get('X');
+x4 = dyn.get('Y');
 
 
 figure(1);
 hold on;
+plot(x1,x2);
+plot(x3,x4);
+
+
+figure(2);
+hold on;
 plot(time, x1);
 plot(time, x2);
-plot(time, x3);
-plot(time, x4);
+plot(time2, x3);
+plot(time2, x4);
+%legend("X","Y", "Ey", "Eo");
